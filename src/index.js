d32f62cf75289c469a781a764f6bd4bd3510db8e
Daniil Komov <daniil.komov@student.lut.fi>
2022-10-16 16:45:43 +0000

    Update 18 files

--- src/index.js
+++ src/index.js
@@ -2,58 +2,208 @@ import "./styles.css";
 
 import { Chart } from "frappe-charts/dist/frappe-charts.esm.js";
 
-new Chart("#chart", {
-  // or DOM element
-  data: {
-    labels: [
-      "12am-3am",
-      "3am-6am",
-      "6am-9am",
-      "9am-12pm",
-      "12pm-3pm",
-      "3pm-6pm",
-      "6pm-9pm",
-      "9pm-12am"
+var Area= "SSS"
+
+if(document.readyState !== "loading") {
+    console.log("Document is ready!");
+    initCode();
+} else {
+    document.addEventListener("DOMContentLoaded", function() {
+        console.log("Document is ready after waiting!");
+        initCode();
+    })
+}
+
+
+
+
+
+function initCode() {
+
+const addAreaButton = document.getElementById("submit-area");
+
+addAreaButton.addEventListener("click", function() {
+    
+    
+    const Area = document.getElementById("submit-area").value
+    
+    
+    console.log(area)
+    
+    
+    
+    
+    
+    
+    
+    
+   
+    
+})
+}
+
+
+
+
+
+
+function JsonQuery(Area) {
+
+const jsonQuery = {
+    "query": [
+        {
+            "code": "Vuosi",
+            "selection": {
+                "filter": "item",
+                "values": [
+                    "2000",
+                    "2001",
+                    "2002",
+                    "2003",
+                    "2004",
+                    "2005",
+                    "2006",
+                    "2007",
+                    "2008",
+                    "2009",
+                    "2010",
+                    "2011",
+                    "2012",
+                    "2013",
+                    "2014",
+                    "2015",
+                    "2016",
+                    "2017",
+                    "2018",
+                    "2019",
+                    "2020",
+                    "2021"
+                ]
+            }
+        },
+        {
+            "code": "Alue",
+            "selection": {
+                "filter": "item",
+                "values": [
+                  Area,
+                  
+                  
+                  
+                 
+                ]
+            }
+        },
+        {
+            "code": "Tiedot",
+            "selection": {
+                "filter": "item",
+                "values": [
+                    "vaesto"
+                ]
+            }
+        }
     ],
+    "response": {
+        "format": "json-stat2"
+    }
+}
 
-    datasets: [
-      {
-        name: "Some Data",
-        chartType: "bar",
-        values: [25, 40, 30, 35, 8, 52, 17, -4]
-      },
-      {
-        name: "Another Set",
-        chartType: "bar",
-        values: [25, 50, -10, 15, 18, 32, 27, 14]
-      },
-      {
-        name: "Yet Another",
-        chartType: "line",
-        values: [15, 20, -3, -15, 58, 12, -17, 37]
-      }
-    ],
 
-    yMarkers: [{ label: "Marker", value: 70, options: { labelPos: "left" } }],
-    yRegions: [
-      { label: "Region", start: -10, end: 50, options: { labelPos: "right" } }
-    ]
-  },
 
-  title: "My Awesome Chart",
-  type: "axis-mixed", // or 'bar', 'line', 'pie', 'percentage'
-  height: 300,
-  colors: ["purple", "#ffa3ef", "light-blue"],
-  axisOptions: {
-    xAxisMode: "tick",
-    xIsSeries: true
-  },
-  barOptions: {
-    stacked: true,
-    spaceRatio: 0.5
-  },
-  tooltipOptions: {
-    formatTooltipX: (d) => (d + "").toUpperCase(),
-    formatTooltipY: (d) => d + " pts"
+return jsonQuery
+
+
+}
+
+
+
+
+
+
+
+
+
+const getData = async (Area) => {
+    
+  const url = "https://statfin.stat.fi/PxWeb/api/v1/en/StatFin/synt/statfin_synt_pxt_12dy.px"
+
+  const res = await fetch(url, {
+      method: "POST",
+      headers: {"content-type": "application/json"},
+      body: JSON.stringify(await JsonQuery(Area))
+  })
+  if(!res.ok) {
+      return;
   }
-});
+  const data = await res.json()
+
+  return data
+}
+
+
+
+const buildChart = async (Area) => {
+    
+  const data = await getData(Area)
+  //console.log(data)
+
+  const years = Object.values(data.dimension.Vuosi.category.label);
+  const regions = Object.values(data.dimension.Alue.category.label);
+  const population = data.value;
+  
+  console.log(data)
+  
+  
+  
+  
+
+  
+  
+
+  regions.forEach((region, index) => {
+    
+      let valuesList = []
+      for(let i = 0; i < years.length; i++) {
+          valuesList.push(population[i])
+      }
+      regions[index] = {
+          name: region,
+          values: valuesList
+      }
+  })
+
+  
+
+  const chartData = {
+      labels: years,
+      datasets: regions,
+  }
+
+
+  const chart = new Chart("#chart", {
+      title: "Finnish population growth",
+      data: chartData,
+      type: "line",
+      height: 450,
+      colors: ['#eb5146'],
+      /*barOptions: {
+          stacked: 1
+      },*/
+      lineOptions: {
+          hideDots: 1,
+          regionFill: 0
+      }
+
+  })
+
+
+
+}
+
+
+buildChart(Area)
+
+
+
+
